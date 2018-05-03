3df7164c2e906abad887aa34367a2d1a367bfa29
jbalcita <jbalcita@cj.com>
2018-05-03 09:12:23 -0700

    chapter one exercises

---
+++ chapter_one.ex
@@ -0,0 +1,16 @@
+defmodule MacroOne do
+
+  defmacro unless(expression, do: block) do
+    quote do
+      case unquote(expression) do
+        x when x in [nil, false] -> unquote(block)
+        _ -> nil
+      end
+    end
+  end
+
+  defmacro raw(expression) do
+    Macro.escape(expression)
+  end
+
+end
