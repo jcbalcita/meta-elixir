421df0d8c8323676ae9e35182fe465573db739cf
jbalcita <jbalcita@cj.com>
2018-07-17 16:26:47 -0700

    figured out how to create functions with dynamic names

--- chapter3/generating_functions.ex
+++ chapter3/generating_functions.ex
@@ -1,5 +1,4 @@
 defmodule Mime do
-
   @external_resource mimes_path = Path.join([__DIR__, "mimes.txt"])
 
   for line <- File.stream!(mimes_path, [], :line) do
@@ -7,13 +6,18 @@ defmodule Mime do
       line
       |> String.split("\t")
       |> Enum.map(&String.trim(&1))
+
     extensions = String.split(rest, ~r/,\s?/)
 
     def exts_from_type(unquote(type)), do: unquote(extensions)
     def type_from_ext(ext) when ext in unquote(extensions), do: unquote(type)
+
+    Enum.each(extensions, fn e ->
+      def unquote(:"#{String.trim(e, ".")}")(), do: unquote(type)
+    end)
   end
 
-    def exts_from_type(_type), do: []
-    def type_from_ext(_ext), do: nil
-    def valid_type?(type), do: exts_from_type(type) |> Enum.any?
+  def exts_from_type(_type), do: []
+  def type_from_ext(_ext), do: nil
+  def valid_type?(type), do: exts_from_type(type) |> Enum.any?()
 end
